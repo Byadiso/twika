5462dd4f0dfb4b0d59a773fbc6ccea4987b67c47
Byadiso <byadiso@gmail.com>
2021-05-15 10:59:31 +0200

    Init

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+const mongoose = require('./database/db');
+
+const session = require('express-session');
+
+
+
+
+const middleware = require('./middleware');
+const port = 3002;
+const message = console.log('server listening on port ' + port);
+
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+//for serving our static files
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "yes-yeso",
+    resave: true,
+    saveUninitialized: false
+}));
+
+//routes
+const loginRoutes = require('./routes/loginRoutes');
+const registerRoutes = require('./routes/registerRoutes');
+
+
+app.use('/login', loginRoutes);
+app.use('/register', registerRoutes);
+
+app.get("/", middleware.requireLogin, (req,res, next)=>{
+
+    var payload = {
+        pageTitle:"Home",
+        userLoggedIn: req.session.user
+    }
+    res.status(200).render("home", payload)
+});
+
+
+// run our server 
+
+const server = app.listen(port, ()=>{
+    message;
+});
+
---
+++ schemas/UserSchema.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const crypto = require ('crypto');
+
+
+const {ObjectId } = mongoose.Schema;
+// const uuidv1 = require('uuid/v1');
+// const { v1: uuidv1 } = require('uuid');
+// const { strict } = require('assert');
+
+const userSchema = new mongoose.Schema({
+    firstName:{ type:String,trim:true,required:true, maxlength:32  },
+    lastName:{type:String,trim:true,required:true, unique: true },
+    username:{type:String,trim:true,required:true, unique: true },
+    email:{type:String,trim:true,required:true, unique: true },
+    password:{type:String,trim:true,required:true, unique: true },
+    profilePic:{type:String, default: "/images/profilePic.png" },
+
+    // hashed_password:{
+    //     type:String,
+    //     required:true,
+      
+    // },
+    // photo: {
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    // about:{
+    //     type:String,
+    //     trim:true
+    //    },
+    // following: [{ type: ObjectId, ref: "USer" }],
+    // followers: [{ type: ObjectId, ref: "USer" }],
+    // salt: String,
+    // role:{
+    //        type: Number,
+    //        default: 0
+    //    },
+    // history:{
+    //        type:Array,
+    //        default: []
+    //    }
+    
+    }, 
+    { timestamps: true }
+);
+
+
+
+
+// // virtual fields;
+// userSchema.virtual('password')
+// .set(function(password) {
+//     this._password = password;
+//     this.salt= uuidv1();
+//     this.hashed_password = this.encryptPassword(password);
+// })
+// .get(function(){
+//     return this._password
+// })
+
+
+// userSchema.methods = {
+//     authenticate: function(plainText){
+//         return this.encryptPassword(plainText) === this.hashed_password;
+//     },
+
+//     encryptPassword: function(password){
+//         if(!password) return "";
+//         try {
+//             return crypto.createHmac('sha1',this.salt)
+//                         .update(password)
+//                         .digest('hex');
+//         } catch (err) {
+//             return "";
+//         }
+//     }
+// };
+
+
+var User = mongoose.model("USer", userSchema);
+
+module.exports = User;
